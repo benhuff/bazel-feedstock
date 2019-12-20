086e4104fc6f83745a0d77307660bc15e2512431
benhuff <benhuff10@gmail.com>
2019-12-20 17:41:00 -0500

    CLANG_LINK_OBJC_RUNTIME = NO;

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,10 @@
 set -v -x
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    set CLANG_LINK_OBJC_RUNTIME=NO
+    set CLANG_LINK_OBJC_RUNTIME = NO;
     sh compile.sh
     mv output/bazel $PREFIX/bin
-    chmod +x $PREFIX/bin/bazel
 else
     sh compile.sh
     mv output/bazel $PREFIX/bin
-    chmod +x $PREFIX/bin/bazel
 fi
