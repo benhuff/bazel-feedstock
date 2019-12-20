49fcfbd3febe650ae2bf97670f4dc96fee8e239c
benhuff <benhuff10@gmail.com>
2019-12-20 17:56:12 -0500

    cmake osx deployment target

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -v -x
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     set CLANG_LINK_OBJC_RUNTIME = NO;
+    set CMAKE_OSX_DEPLOYMENT_TARGET = ${CMAKE_SYSTEM_VERSION}
     sh compile.sh
     mv output/bazel $PREFIX/bin
 else
