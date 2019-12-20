376dcd42dc8c75ecba0c749f3a262e8f4e03bab0
benhuff <benhuff10@gmail.com>
2019-12-20 18:08:40 -0500

    abcdefg

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 set -v -x
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo ${CMAKE_SYSTEM_VERSION}
     set CLANG_LINK_OBJC_RUNTIME = NO;
     set CMAKE_OSX_DEPLOYMENT_TARGET = ${CMAKE_SYSTEM_VERSION};
     sh compile.sh
