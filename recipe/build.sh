26edac4734c0e75a6e3e08db4cd0bf17fa13ebd7
benhuff <benhuff10@gmail.com>
2019-12-20 16:51:02 -0500

    echo prefix/bin contents

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 set -v -x
-sh compile.sh
-mv output/bazel $PREFIX/bin
-chmod +x $PREFIX/bin/bazel
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    set CLANG_LINK_OBJC_RUNTIME=NO
+    sh compile.sh
+    mv output/bazel $PREFIX/bin
+    chmod +x $PREFIX/bin/bazel
+else
+    sh compile.sh
+    mv output/bazel $PREFIX/bin
+    chmod +x $PREFIX/bin/bazel
+fi
