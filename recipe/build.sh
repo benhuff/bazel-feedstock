bb36abb223c867d5b38f8cba1e96ea328f28767f
benhuff <benhuff10@gmail.com>
2019-12-20 19:04:17 -0500

    cmake target = cxx

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,9 @@
 set -v -x
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo ${CMAKE_SYSTEM_VERSION}
+    echo ${CXX}
     set CLANG_LINK_OBJC_RUNTIME = NO;
-    set CMAKE_OSX_DEPLOYMENT_TARGET = ${CMAKE_SYSTEM_VERSION};
+    set CMAKE_OSX_DEPLOYMENT_TARGET = ${CXX};
     sh compile.sh
     mv output/bazel $SRC_DIR
 else
