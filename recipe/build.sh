738452808a4bb32a2f0e29fc570576c4088959a3
benhuff <benhuff10@gmail.com>
2020-01-02 14:12:38 -0500

    statically link stdlib linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     echo ${CXX}
     set CLANG_LINK_OBJC_RUNTIME = NO;
     set CMAKE_OSX_DEPLOYMENT_TARGET = ${CXX};
-    sh compile.sh
+    ./compile.sh
     mv output/bazel $PREFIX/bin
 else
-    export BAZEL_LINKOPTS="-static-libgcc:-static-libstdc++:-l%:libstdc++.a:-lm:-Wl,--disable-new-dtags"
-    sh compile.sh
+    ./compile.sh
     mv output/bazel $PREFIX/bin
-    readelf -d $PREFIX/bin/bazel
 fi
