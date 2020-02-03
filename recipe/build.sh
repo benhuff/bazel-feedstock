9176944aa5d77c3627b0bf59f33dbb562a183012
benhuff <benhuff10@gmail.com>
2020-02-03 14:10:54 -0500

    export cppflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -v -x
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    export CPPFLAGS="-D_FORTIFY_SOURCE=2 -mmacosx-version-min=10.9 -isystem $PREFIX/include"
+    export CPPFLAGS="-D_FORTIFY_SOURCE=2 -mmacosx-version-min=10.12 -isystem $PREFIX/include"
     ./compile.sh
     mkdir -p $PREFIX/bin/
     mv output/bazel $PREFIX/bin
