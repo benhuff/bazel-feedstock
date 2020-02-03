ac60bc54733ec716f6cf0929fecf1712fd0f5944
benhuff <benhuff10@gmail.com>
2020-02-03 14:09:00 -0500

    export cppflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -v -x
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    set CMAKE_OSX_DEPLOYMENT_TARGET = '10.12'
+    export CPPFLAGS="-D_FORTIFY_SOURCE=2 -mmacosx-version-min=10.9 -isystem $PREFIX/include"
     ./compile.sh
     mkdir -p $PREFIX/bin/
     mv output/bazel $PREFIX/bin
