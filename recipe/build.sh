0849fd0a6ac37fff43288df238580ccd089f6880
benhuff <benhuff10@gmail.com>
2020-02-03 12:11:44 -0500

    add mkdir

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,5 +10,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     mv output/bazel $PREFIX/bin
 else
     ./compile.sh
-#    mv output/bazel $PREFIX/bin
+    mkdir -p $PREFIX/bin/
+    mv output/bazel $PREFIX/bin
 fi
