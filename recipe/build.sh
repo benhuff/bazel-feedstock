89578ffeed3c97bbd4bcb92a47c887f2a60c9ad1
benhuff <benhuff10@gmail.com>
2020-02-04 14:27:20 -0500

    try v2 again

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,17 @@
 
 set -v -x
 
-./compile.sh
-mkdir -p $PREFIX/bin/
-mv output/bazel $PREFIX/bin
+if [ $(uname) == Darwin ]; then
+    # Darwin has only clang, must use clang++ from clangdev
+    # All these must be picked up from $PREFIX/bin
+    export CC=clang
+    export CXX=clang++
+    export MACOSX_DEPLOYMENT_TARGET=10.12
+    ./compile.sh
+    mkdir -p $PREFIX/bin/
+    mv output/bazel $PREFIX/bin
+else
+    ./compile.sh
+    mkdir -p $PREFIX/bin/
+    mv output/bazel $PREFIX/bin
+fi
