d748c6c8c75c1f06924bade4c6b3857159d2b28a
benhuff <benhuff10@gmail.com>
2020-02-04 14:41:31 -0500

    llvm 6.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,17 +2,6 @@
 
 set -v -x
 
-if [ $(uname) == Darwin ]; then
-    # Darwin has only clang, must use clang++ from clangdev
-    # All these must be picked up from $PREFIX/bin
-    export CC=clang
-    export CXX=clang++
-    export MACOSX_DEPLOYMENT_TARGET=10.12
-    ./compile.sh
-    mkdir -p $PREFIX/bin/
-    mv output/bazel $PREFIX/bin
-else
-    ./compile.sh
-    mkdir -p $PREFIX/bin/
-    mv output/bazel $PREFIX/bin
-fi
+./compile.sh
+mkdir -p $PREFIX/bin/
+mv output/bazel $PREFIX/bin
