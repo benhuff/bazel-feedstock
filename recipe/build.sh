96825d03767483e9a81489561dfab6fa82a8b7ff
benhuff <benhuff10@gmail.com>
2020-02-04 14:47:34 -0500

    strict build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,16 @@
 
 set -v -x
 
-./compile.sh
-mkdir -p $PREFIX/bin/
-mv output/bazel $PREFIX/bin
+if [ $(uname) == Darwin ]; then
+    # Darwin has only clang, must use clang++ from clangdev
+    # All these must be picked up from $PREFIX/bin
+    export CC=x86_64-apple-darwin14.0.0-clang
+    export CXX=x86_64-apple-darwin14.0.0-clang++
+    ./compile.sh
+    mkdir -p $PREFIX/bin/
+    mv output/bazel $PREFIX/bin
+else
+    ./compile.sh
+    mkdir -p $PREFIX/bin/
+    mv output/bazel $PREFIX/bin
+
