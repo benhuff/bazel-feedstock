668a40a2b28747d2ddadd11b30198e682001d0b2
benhuff <benhuff10@gmail.com>
2020-02-04 13:15:53 -0500

    reset build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,6 @@
 
 set -v -x
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    export MACOSX_DEPLOYMENT_TARGET=10.12
-    export CPPFLAGS="-D_FORTIFY_SOURCE=2 -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET -isystem $PREFIX/include"
-    ./compile.sh
-    mkdir -p $PREFIX/bin/
-    mv output/bazel $PREFIX/bin
-else
-    ./compile.sh
-    mkdir -p $PREFIX/bin/
-    mv output/bazel $PREFIX/bin
-fi
+./compile.sh
+mkdir -p $PREFIX/bin/
+mv output/bazel $PREFIX/bin
